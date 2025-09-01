5ddb6e23729e2f743aa4852ffefe1e91439401fc
itskenedwards <149261252+itskenedwards@users.noreply.github.com>
2025-09-01 14:05:31 -0400

    Updated AuthForm.tsx

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -154,8 +154,8 @@ export default function AuthForm({ mode, onModeChange, onForgotPassword, showDat
       <div className="max-w-md w-full space-y-8">
         <div>
           <div className="flex justify-center items-center space-x-4">
-            <Home className="h-32 w-32 text-blue-600" style="font-size:120%" />
-            <h1 className="text-center text-2xl font-bold text-gray-900">
+            <Home className="h-32 w-32 text-blue-600" />
+            <h1  style="font-size:120%" className="text-center text-2xl font-bold text-gray-900">
               HOA Homeowner Violation Tracking System
             </h1>
           </div>
